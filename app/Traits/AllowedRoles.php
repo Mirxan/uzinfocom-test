f0a1c848094454e29f7915162c5a4ec77f61d307
Muhammadamin <mirxan085@gmail.com>
2024-05-27 17:54:31 +0500

    added allowedRoles scope to validate for needed actions

--- app/Traits/AllowedRoles.php
+++ app/Traits/AllowedRoles.php
@@ -11,17 +11,17 @@ trait AllowedRoles
     /**
      * Scope a query to only allowed roles.
      */
-    public function scopeAllowedRoles(Builder $query, string $action = 'delete'): void
+    public function scopeAllowedRoles(Builder $query, string $property = 'deleteAllowedRoles'): void
     {
         if (auth()->user()->isAdmin()) $query;
 
-        if (!property_exists($this, 'deleteAllowedRoles')) {
+        if (!property_exists($this, $property)) {
             throw new Exception('Model {{' . class_basename($this) . '}} doesn\'t have the {{$deleteAllowedRoles}} property', 500);
         }
 
         $currentUserRoleNames = auth()->user()?->roles?->pluck('name')->toArray();
         $currentUserRoleIds = auth()->user()?->roles?->pluck('id')->toArray();
-        $deleteAllowedRoles = collect($this->deleteAllowedRoles)->filter(fn ($item, $key) => in_array($key, $currentUserRoleNames));
+        $deleteAllowedRoles = collect($this->$property)->filter(fn ($item, $key) => in_array($key, $currentUserRoleNames));
         $role_ids = Role::whereIn('name', collect($deleteAllowedRoles)->flatten())->pluck('id')->concat($currentUserRoleIds);
 
         $query->whereHas('user', function ($q) use ($role_ids) {
